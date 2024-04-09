4195f73ed2a270bb0e6f9146866e9840961df6ac
kenetkit <jankenneth363@gmail.com>
2024-04-09 15:20:54 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 })
-
-module.exports = {
-  publicPath: '/midterm/',
-  outputDir: 'dist',
-  assetsDir: 'assets',
-  indexPath: 'index.html',
-  filenameHashing: true,
-}
